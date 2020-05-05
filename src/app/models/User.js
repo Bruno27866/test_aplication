ad0da0a68f5ae9f33cfa2925485b5d045f92d4a1
Bruno Alencar Amaral <b.alencar_amaral@yahoo.com>
2020-05-05 15:54:23 -0300

    Primeiro commit

---
+++ src/app/models/User.js
@@ -0,0 +1,40 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  /* Essa função esta sendo utilizada pois não existe o campo de "avatar_id"
+  pré-definido no modelo de usuário, é importante lembrar de adicinar esse campo
+  caso vá utilizar esse tipo de informação */
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '91db02384a06d280e4d4e527377b48fb',
+  expiresIn: '7d',
+};
