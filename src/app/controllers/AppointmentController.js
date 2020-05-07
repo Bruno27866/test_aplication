a12ffcb449ab2a48901f3d6594ce106613d64d10
Bruno Alencar Amaral <b.alencar_amaral@yahoo.com>
2020-05-07 17:37:47 -0300

    10º commit: erros não resolvidos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
-// import pt from 'date-fns/locale/pt-BR';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointments';
@@ -46,7 +46,7 @@ class AppointmentController {
      * Check if provider_id is a provider
      * Verifica se o id do prestador é de um prestador
      */
-    /* const isProvider = await User.findOne({
+    const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
     if (!isProvider) {
@@ -59,14 +59,14 @@ class AppointmentController {
      * Verifica se não é uma data passada
      */
     const hourStart = startOfHour(parseISO(date));
-    /* if (isBefore(hourStart, new Date())) {
+    if (isBefore(hourStart, new Date())) {
       return res.status(400).json({ error: 'Past date are not premitted' });
     }
     /**
      * Check date availability
      * Verifica a disponibilidade de data e hora
      */
-    /* const checkAvailability = await Appointment.findOne({
+    const checkAvailability = await Appointment.findOne({
       where: {
         provider_id,
         canceled_at: null,
@@ -78,12 +78,29 @@ class AppointmentController {
         .status(400)
         .json({ error: 'Appointment date is not available' });
     }
-*/
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
       date: hourStart,
     });
+
+    /*
+     *Notify appointment provider
+     *Notifica prestador de serviço
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locate: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
+    });
+
     return res.json(appointment);
   }
 }
