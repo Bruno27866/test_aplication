b852f9b528068727a40535fb3b36ab30ff192783
Bruno Alencar Amaral <b.alencar_amaral@yahoo.com>
2020-05-11 15:06:05 -0300

    15.3º commit

--- src/routes.js
+++ src/routes.js
@@ -15,14 +15,22 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/Schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
-routes.put('/Notifications/:id', NotificationController.update);
+routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
