94ff9db64c6e903ee8be40846688108c144e9efa
Bruno Alencar Amaral <b.alencar_amaral@yahoo.com>
2020-05-11 16:23:29 -0300

    16ºcommit: passar para o caderno

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6afed108bd2eda',
+    pass: 'dc7d7cb42040f9',
+  },
+  default: {
+    from: 'BTech informática <noreplay@bruno.email.com>',
+  },
+};
